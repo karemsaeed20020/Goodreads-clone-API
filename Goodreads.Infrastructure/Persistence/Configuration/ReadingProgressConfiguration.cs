004354dc174bc879e0e0b4efb1f8f2fe8fdbad65
Karem Saeed <95093124+karemsaeed20020@users.noreply.github.com>
2025-11-27 15:13:20 +0200

    Adding ReadingProgress Congiuration

---
+++ Goodreads.Infrastructure/Persistence/Configuration/ReadingProgressConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Goodreads.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Goodreads.Infrastructure.Persistence.Configuration
+{
+    public class ReadingProgressConfiguration : IEntityTypeConfiguration<ReadingProgress>
+    {
+        public void Configure(EntityTypeBuilder<ReadingProgress> builder)
+        {
+            builder.HasKey(rp => new { rp.UserId, rp.BookId });
+            builder.HasOne(rp => rp.Book)
+                .WithMany()
+                .HasForeignKey(rp => rp.BookId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
