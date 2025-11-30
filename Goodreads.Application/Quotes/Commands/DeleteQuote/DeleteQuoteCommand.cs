b6f7d34d5244357ca1956965d6029050150d92d9
Karem Saeed <95093124+karemsaeed20020@users.noreply.github.com>
2025-11-30 18:53:31 +0200

    Delete Quote and Add RequireQuoteAuthorization

---
+++ Goodreads.Application/Quotes/Commands/DeleteQuote/DeleteQuoteCommand.cs
@@ -0,0 +1,11 @@
+﻿using Goodreads.Application.Common.Interfaces.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Goodreads.Application.Quotes.Commands.DeleteQuote
+{
+    public record DeleteQuoteCommand(string QuoteId) : IRequest<Result>, IRequireQuoteAuthorization;
+}
---
+++ Goodreads.Infrastructure/Authorization/QuoteAuthorizationService.cs
@@ -0,0 +1,31 @@
+﻿using Goodreads.Application.Common.Interfaces;
+using Goodreads.Application.Common.Interfaces.Authorization;
+using Goodreads.Domain.Constants;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Goodreads.Infrastructure.Authorization
+{
+    internal class QuoteAuthorizationService(IUnitOfWork unitOfWork, IUserContext userContext) : IQuoteAuthorizationService
+    {
+        public async Task<bool> IsOwnerOrAdminAsync(string quoteId)
+        {
+            var userId = userContext.UserId;
+            if (string.IsNullOrEmpty(userId))
+                return false;
+
+            // Check if user is admin
+            if (userContext.IsInRole(Roles.Admin))
+                return true;
+
+            // Check if user is the owner of the quote
+            var quote = await unitOfWork.Quotes.GetByIdAsync(quoteId);
+            if (quote is null || quote.CreatedByUserId != userId) return false;
+
+            return quote.CreatedByUserId == userId;
+        }
+    }
+}
